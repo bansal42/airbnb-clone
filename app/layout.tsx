627601ddee0be13c928bd957783363cca9e83a58
Khushi Bansal <138283670+bansal42@users.noreply.github.com>
2024-03-28 16:18:47 +0530

    login functionality

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import ClientOnly from "./components/ClientOnly";
 import RegisterModal from "./components/modals/RegisterModal";
+import LoginModal from "./components/modals/LoginModal";
 import Navbar from "./components/navbar/Navbar";
 import "./globals.css";
 import { Nunito } from "next/font/google";
 import ToasterProvider from "./providers/ToasterProvider";
+import getCurrentUser from "./actions/getCurrentUser";
 
 
 export const metadata = {
@@ -15,18 +17,21 @@ const font = Nunito({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const currentUser = await getCurrentUser();
+
   return (
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
-          <Navbar />
+          <Navbar currentUser= {currentUser} />
         </ClientOnly>
       {children}
       </body>
